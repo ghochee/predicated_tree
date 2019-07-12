5f3307361317db4534bc9cefa5dad4160747318d
Aditya Dalmia <dalmia.aditya@gmail.com>
2019-07-12 18:16:40 +0530

    predicated_tree: Update benchmark test.
    Simplify and correct metric collection for the predicated tree test.

--- test/predicated_tree_bm_test.cpp
+++ test/predicated_tree_bm_test.cpp
@@ -8,56 +8,71 @@
 using namespace std;
 
 template <typename T>
-struct more_even {
-    bool operator()(const T a, const T b) const {
-        return (a ^ (a - 1)) > (b ^ (b - 1));
-    }
-};
-
-static void Insert(benchmark::State &state) {
-    mt19937 rng(0);
-    uniform_int_distribution<std::mt19937::result_type> dist(100, 100'000);
-    predicated_tree<uint32_t, comparator<uint32_t, more_even<uint32_t>,
-                                         std::less<uint32_t>>> p;
-
-        for (auto _ : state) {
-        state.PauseTiming();
-        raw_tree<uint32_t> t(1 << 29);
-        accessor<raw_tree<uint32_t>> root(t);
-        for (uint32_t i = 0; i < state.range(0); ++i) {
-            p.insert(dist(rng));
-        }
-        state.ResumeTiming();
-
-        for (uint32_t i = 0; i < 1 << 6; ++i) {
-            p.insert(dist(rng));
-        }
-    }
+static bool more_even(const T &a, const T &b) {
+    return (a ^ (a - 1)) > (b ^ (b - 1));
 }
-BENCHMARK(Insert)->DenseRange(0, 1 << 15, 1024)->ThreadPerCpu();
 
-static void Erase(benchmark::State &state) {
-    mt19937 rng(0);
-    uniform_int_distribution<std::mt19937::result_type> dist(100, 100'000);
-    predicated_tree<uint32_t, comparator<uint32_t, more_even<uint32_t>,
-                                         std::less<uint32_t>>> p;
-    p.insert(1 << 29);
+static void Find(benchmark::State &state) {
+    std::random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<std::mt19937::result_type> dist(100, 100'000'000);
+    auto c = make_comparator<uint32_t, more_even, std::less<uint32_t>>();
+
+    predicated_tree<uint32_t, decltype(c)> p;
+    set<uint32_t> inserted;
+    for (uint32_t i = 0; i < state.range(0); ++i) {
+        uint32_t value = dist(rng);
+        inserted.insert(value);
+        p.insert(uint32_t(value));
+    }
 
     for (auto _ : state) {
         state.PauseTiming();
-        set<uint32_t> erasables;
-        for (uint32_t i = 0; i < state.range(0); ++i) {
-            uint32_t value = dist(rng);
-            if (erasables.size() < 1 << 6) { erasables.insert(value); }
-            p.insert(std::move(value));
+        for (auto it = inserted.begin(); it != inserted.end(); ++it) {
+            state.ResumeTiming();
+            benchmark::DoNotOptimize(p.find(*it));
+            state.PauseTiming();
         }
         state.ResumeTiming();
+    }
+}
+BENCHMARK(Find)->Range(1, 1 << 18)->ThreadPerCpu()->Iterations(20);
 
-        for (auto it = erasables.begin(); it != erasables.end(); ++it) {
+static void Insert(benchmark::State &state) {
+    std::random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<std::mt19937::result_type> dist(100, 100'000);
+    auto c = make_comparator<uint32_t, more_even, std::less<uint32_t>>();
+
+    for (auto _ : state) {
+        predicated_tree<uint32_t, decltype(c)> p;
+        for (uint32_t i = 0; i < state.range(0); ++i) { p.insert(dist(rng)); }
+    }
+}
+BENCHMARK(Insert)->Range(1, 1 << 18)->ThreadPerCpu()->Iterations(20);
+
+static void Erase(benchmark::State &state) {
+    std::random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<std::mt19937::result_type> dist(100, 100'000);
+    auto c = make_comparator<uint32_t, more_even, std::less<uint32_t>>();
+    predicated_tree<uint32_t, decltype(c)> p(c);
+
+    for (auto _ : state) {
+        state.PauseTiming();
+        set<uint32_t> inserted;
+        for (uint32_t i = 0; i < state.range(0); ++i) {
+            uint32_t value = dist(rng);
+            inserted.insert(value);
+            p.insert(std::move(value));
+        }
+
+        state.ResumeTiming();
+        for (auto it = inserted.begin(); it != inserted.end(); ++it) {
             p.erase(p.find(*it));
         }
     }
 }
-BENCHMARK(Erase)->DenseRange(0, 1 << 15, 1024)->ThreadPerCpu();
+BENCHMARK(Erase)->Range(1, 1 << 18)->ThreadPerCpu()->Iterations(20);
 
 BENCHMARK_MAIN();
