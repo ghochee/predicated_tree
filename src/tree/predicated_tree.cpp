e380d031da6f957686a529ea51a3433f297ced69
Aditya Dalmia <dalmia.aditya@gmail.com>
2019-07-13 15:04:38 +0530

    src: for useful and demonstrative executables.
    src/ directory will contain executable targets which might be building
    useful command line tools. This change adds a predicated_tree target
    which builds and prints a simple tree. Related plumbing changes are
    made.

---
+++ src/tree/predicated_tree.cpp
@@ -0,0 +1,27 @@
+#include "tree.h"
+#include "tree/util/predicates.h"
+#include "tree/util/format.h"
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main(int, char **) {
+    mt19937 rng(0);
+    uniform_int_distribution<std::mt19937::result_type> dist(1, 100'000'000);
+
+    auto c = make_comparator<uint32_t, more_even, std::less<uint32_t>>();
+    predicated_tree<uint32_t, decltype(c)> p(c);
+
+    vector<uint32_t> inserted_values;
+    for (uint32_t i = 0; i < 100; ++i) {
+        uint32_t value = dist(rng);
+        inserted_values.push_back(value);
+        p.insert(std::move(value));
+    }
+    cout << "Built a tree of size: " << p->size() << endl;
+    cout << p.node() << endl;
+
+    return 0;
+}
